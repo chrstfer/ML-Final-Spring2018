a1b5415cef86be63c6ab83bcfe3e213ff39b8e32
Chris Carrigan Brolly <chrstfr@gmail.com>
2018-04-13 06:12:21 -0400

    From toy to tool
    at least it all runs. Performance is abysmal. Don't know why.

--- testingbed/train-model.R
+++ testingbed/train-model.R
@@ -1,5 +1,4 @@
 library(sigmoid)
-
 getLabels <- function(lbldb, nget=0) {
     magic  <- readBin(lbldb, what="integer", n=1, endian="big", size=4)
     if(magic != 2049)
@@ -13,7 +12,6 @@ getLabels <- function(lbldb, nget=0) {
     close(lbldb)
     return(labels)
 }
-
 getImages <- function(imgdb, nget=0, progress=FALSE) {
     magic  <- readBin(imgdb, what="integer", n=1, endian="big", size=4)
     ## if(magic != 2049)
@@ -40,136 +38,144 @@ getImages <- function(imgdb, nget=0, progress=FALSE) {
 }
 
 ## Works
-tr.labels <- as.vector(getLabels(gzfile("../datasets/training/labels", "rb"), nget=256))
-tr.images <- getImages(gzfile("../datasets/training/images", # data's filename
-                              "rb"), # read it as binary
-                       ## Get 256 of the entries
-                       nget=256, progress=TRUE)
-tr.im.matrix <- do.call("rbind", # create rows out of the input data
-                        lapply(tr.images, as.vector)) # transform each image
+indata.labels <- as.vector(getLabels(gzfile("../datasets/training/labels", "rb"), nget=256))
+indata.images <- getImages(gzfile("../datasets/training/images", # data's filename
+                                  "rb"), # read it as binary
+                           ## Get 256 of the entries
+                           nget=256, progress=TRUE)
+indata.im.matrix <- do.call("rbind", # create rows out of the input data
+                            lapply(indata.images, as.vector)) # transform each image
                                         # matrix into a vector
 
-tr.df <- cbind(as.factor(tr.labels), tr.im.matrix) # now create a data frame
+indata <- cbind(as.factor(indata.labels), indata.im.matrix) # now create a data frame
 
-model.gen.annc <- function(layers,
-                           training.data,
-                           training.labels,
-			   debug=TRUE) {    
-    .lengths <- layers    
-    .nlayers <- length(.lengths)
+ts.idx <- sample(length(indata.labels),2*sqrt(length(indata.labels))) ## 4:1 tr:ts (for now)
 
-    if(debug) print(paste("Number of Layers:", n.layers))
-    if(debug) print(paste("Layer Lengths:",    toString(.lengths)))
-    
-    model <- train(training.data, training.labels)
-    class(model) <- "model.ann.classifier"
-    return(model) 
-}
+ts.df <- indata[ts.idx,]
+ts.im <- ts.df[-1,]
+ts.lb <- ts.df[1,]
+ts.images <- indata.images[ts.idx]
+ts.labels <- indata.labels[ts.idx]
 
-
-normalize <- function(x){return(x/sum(x))}
-activate <- function(node)
-    return(matrix(1/(1+exp(-node))))
-sigprime <- function(node)
-    return(matrix(activate(node)*(1 - activate(node))))
-
-
-train <- function(data, labels) { # row-wise dataframe and list
-    .lengths <- c(784,16,8,10)
-    .nlayers <- length(.lengths)
-    errs <- list()
-    
-    .active <- mapply(matrix,
-                      data=1,
-                      ncol=1,
-                      nrow=.lengths)
-    .nodes <- mapply(matrix,
-                     data=1,
-                     ncol=1,
-                     nrow=.lengths)
-    .biases <- mapply(matrix,
-                      data=lapply(.lengths, rnorm),
-                      ncol=1,
-                      nrow=.lengths)
-    .weights <- lapply(1:(.nlayers-1),
-                       function(k) {
-                           matrix(rnorm(.lengths[k+1]*.lengths[k]),
-                                  nrow=.lengths[k+1],
-                                  ncol=.lengths[k])})
-
-    
-
-    
-    ## lapply(tr.df[,-1],thefollowing) <- do once for each and then average?
-    ## split whole db into batches, find average across
-    for(n in 1:nrow(data)) {
-        truth <- rep(0,10)
-        truth[labels[n]+1] <- 1
-        
-        .nodes[[1]] <- matrix(data[n,],ncol=1)
-        .active[[1]] <- activate(.nodes[[1]])
-        ## We could definitely get this faster. Each iteration only depends
-        ## on the previous layer's active, if we could keep passing that
-        ## down the chain
-        .weights[[1]] %*% .active[[1]]
-
-        for(i in 2:(.nlayers)) {
-            print(i)
-            setBreakpoint(srcfile="train-model.R",line=112)
-                                        #print(dim(.weights[[i-1]]))
-                                        #print(dim(.active[[i-1]]))                
-            ##.nodes[[i]] <- .active[[i-1]] %*% .weights[[i-1]] + .biases[[i-1]]               
-            .nodes[[i]] <- (.weights[[i-1]] %*% .active[[i-1]])  + .biases[[i-1]]
-            .active[[i]] <- activate(.nodes[[i]])
-        }
-
-        if(debug)
-            errs[[n]] <- (model$active[[.nlayers]] - truth)
-
-        del <- list()
-        del[[(.nlayers-1)]] <- (.active[[.nlayers]] - truth) * sigprime(.nodes[[.nlayers]])
-        ## Loop throught the rest
-        for(i in seq((.nlayers-1),2,-1)) {
-            del[[i-1]] <- (t(.weights[[i]]) %*% del[[i]]) * sigprime(.nodes[[i]])
-        }
-
-        .weights <- lapply(length(.weights):1,
-                           function(i)
-                               return(.weights[[i]] - learningrate * del[[i]] %*% t(.active[[i]])))
-        
-        .biases <- lapply(length(.weights):1, 
-                          function(i)
-                              return(.biases[[i]] - learningrate * del[[i]]))
-    }
-    
-    trained <- list()
-    trained$nodes <- .nodes
-    trained$weights <- .weights
-    trained$biases <- .biases
-
-    trained$nlayers <- n.layers
-    trained$lengths <- .lengths
-    
-    return(trained)
-}
-
-
+tr.df <- indata[-ts.idx,]
+tr.im <- tr.df[,-1]
+tr.lb <- tr.df[,1]
+tr.images <- indata.images[-ts.idx]
+tr.labels <- indata.labels[-ts.idx]
 
 ## basic layout, 4 layers of 5x1
 set.seed(1)
 
 
+ann <- function(node_lengths,
+                dlist, l,
+                tr.idx, ts.idx,
+                learningrate) {
+    
+    tr.d <- dlist[tr.idx]
+    tr.l <- l[tr.idx]
+    ts.d <-  dlist[ts.idx]
+    ts.l <-  l[ts.idx] 
 
-r1 <- predict(model, c(1,0))
+    model <- new.env()
+    model$lengths <- node_lengths
+    lengths <- model$lengths
+    model$nlayers <- length(model$lengths)
+    nlayers <- model$nlayers
+
+    normalize <- function(x){return(x/sum(x))}
+    activate <- function(node)
+        return(matrix(1/(1+exp(-node))))
+    sigprime <- function(node)
+        return(matrix(activate(node)*(1 - activate(node))))
+    
+    model$debug <- TRUE
+    model$errs <- list()
+    model$biases <- mapply(matrix,
+                           data=lapply(lengths[-1], rnorm),
+                           ncol=1,
+                           nrow=lengths[-1])
+    model$weights <- lapply(1:(nlayers-1),
+                            function(k) {
+                                matrix(rnorm(lengths[k+1]*lengths[k]),
+                                       nrow=lengths[k+1],
+                                       ncol=lengths[k])})
+
+    ## semi-Pure function: references but does not modify its parent env
+    model$predict <- function(input) {
+        active <- list()
+        nodes <- list()
+
+        nodes[[1]] <- as.vector(input)
+        active[[1]] <- activate(nodes[[1]])
+
+        for(i in 2:nlayers) {
+            nodes[[i]] <- (model$weights[[i-1]] %*% active[[i-1]])#  + biases[[i-1]])
+            active[[i]] <- activate(nodes[[i]])
+        }
+                
+        which.max(as.vector(active[[nlayers]]))-1
+    }
+        
+    train <- function(input, label) {
+        truth <- rep(0,10)
+        truth[label+1] <- 1
+
+        active <- list()
+        nodes <- list()
+
+        nodes[[1]] <- as.vector(input)
+        active[[1]] <- activate(nodes[[1]])
+
+        for(i in 2:nlayers) {
+            nodes[[i]] <- (weights[[i-1]] %*% active[[i-1]]) #  + biases[[i-1]]
+            active[[i]] <- activate(nodes[[i]])
+        }
+        
+        del <- list()
+        del[[(nlayers - 1)]] <-  (active[[nlayers]] - truth)* sigprime(nodes[[nlayers]])
+        ## n-1, n-2, .. 3, 2
+        for(i in seq((nlayers-1), 2, -1)) { 
+            del[[i-1]] <- (t(weights[[i]]) %*% del[[i]]) * sigprime(nodes[[i]])
+        }
+                    
+        model$weights <<- lapply(1:length(weights),
+                           function(i)
+                               return(weights[[i]] - learningrate * del[[i]] %*% t(active[[i]])))
+
+        ## model$biases <<- lapply(1:length(biases),
+        ##                   function(i)
+        ##                       return(biases[[i]] - learningrate * del[[i]]))
+            
+        
+    }
+
+    
+    test <- function(inputs, labels) {        
+        preds <- lapply(inputs,model$predict)
+        preds==labels
+    }
+
+    ## Impure functions
+    environment(train) <- model ## MODIFIES ENV
+    environment(model$predict) <- model ## Does not modify env   
+    
+    ## Do initialization
+    model$trained <- mapply(train, tr.d, tr.l)
+    model$tested <- test(ts.d, ts.l)
+
+    return(model)
+}
 
 
 
-r2 <- predict(model, c(1,1))
-r3 <- predict(model, c(0,1))
-r4 <- predict(model, c(0,0))
 
-print("Results ::")
-paste("Results 1: ",r1)
-paste("Results 1: ",r2)
-paste("Results 1: ",r3)
-paste("Results 1: ",r4)
+models <- lapply(seq(0.5,1,0.05),
+       ann,
+       node_lengths=c(784,16,4,10),
+       dlist=indata.images,
+       l=indata.labels,
+       tr.idx=-ts.idx,
+       ts.idx=ts.idx)
+
+lapply(models, function(model) length(which(model$tested)))
